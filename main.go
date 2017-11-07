409b6a905cb036e8c5932e19d0f4c4ab230ad290
DrCalx <matthew.duguay@gmail.com>
2017-11-07 14:33:54 -0500

    Using gin

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+type Album struct {
+	Id string
+	Title string
+	Artist string
+}
+
+// var templates = template.Must(template.ParseFiles("views/layout.html", "views/view.html", "views/edit.html"))
+
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	a := &Album{Id: "1", Title: "Please Please Me", Artist: "The Beatles"}
+// 	renderTemplate(w, "view", a)
+// }
+
+// func editHandler(w http.ResponseWriter, r *http.Request) {
+// 	a := &Album{Id: "1", Title: "Please Please Me", Artist: "The Beatles"}
+// 	renderTemplate(w, "edit", a)
+// }
+
+// func renderTemplate(w http.ResponseWriter, tmpl string, a *Album) {
+// 	t, _ := template.ParseFiles("views/layout.html", "views/"+tmpl+".html")
+// 	err := t.ExecuteTemplate(w, "layout", a)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+// }
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK, 
+			"index.html", 
+			gin.H{
+				"Title": "Record Holder",
+			},
+		)
+	})
+
+	router.Run()
+}
