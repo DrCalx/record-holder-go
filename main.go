dbaa8900e6f2d7453845ba052593903cd1976b71
DrCalx <matthew.duguay@gmail.com>
2017-11-11 11:26:10 -0500

    Clean up main. Remove unused templates

--- main.go
+++ main.go
@@ -6,32 +6,6 @@ import (
 
 var router *gin.Engine
 
-type Album struct {
-	Id string
-	Title string
-	Artist string
-}
-
-// var templates = template.Must(template.ParseFiles("views/layout.html", "views/view.html", "views/edit.html"))
-
-// func viewHandler(w http.ResponseWriter, r *http.Request) {
-// 	a := &Album{Id: "1", Title: "Please Please Me", Artist: "The Beatles"}
-// 	renderTemplate(w, "view", a)
-// }
-
-// func editHandler(w http.ResponseWriter, r *http.Request) {
-// 	a := &Album{Id: "1", Title: "Please Please Me", Artist: "The Beatles"}
-// 	renderTemplate(w, "edit", a)
-// }
-
-// func renderTemplate(w http.ResponseWriter, tmpl string, a *Album) {
-// 	t, _ := template.ParseFiles("views/layout.html", "views/"+tmpl+".html")
-// 	err := t.ExecuteTemplate(w, "layout", a)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 	}
-// }
-
 func main() {
 	router = gin.Default()
 
